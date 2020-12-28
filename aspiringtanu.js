f92d3368700a34f9fe05ac94683ed4b5f9e45e21
Aspiring T@NU <63964708+tanumalakar@users.noreply.github.com>
2020-12-28 23:42:11 +0600

    Add files via upload

---
+++ aspiringtanu.js
@@ -0,0 +1,16 @@
+function btnclr(){
+  document.calculator.display.value="";
+}
+
+function add(){
+  document.calculator.display.value+="+";
+}
+function sub(){
+  document.calculator.display.value+="-";
+}
+function mul(){
+  document.calculator.display.value+="*";
+}
+function div(){
+  document.calculator.display.value+="/";
+}
